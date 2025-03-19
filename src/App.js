7bf258b219a8f083fd191b4eb11f85ba4f1b8d67
Sammy6543 <mohammedsameerrangrej.cs19@aryacollege.in>
2025-03-19 15:16:34 +0530

    created components in react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from "../src/Components/Nav/Navbar";
+import Hero from "../src/Components/Hero/Hero";
+import Service from "../src/Components/Service/Service";
+import Footer from "../src/Components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+
+    <Navbar />
+    <Hero />
+    <Service />
+    <Footer />
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Service/Service.jsx
@@ -0,0 +1,11 @@
+import "./service.css";
+
+const Services = () => {
+    return (
+        <>
+            <h1>Services</h1>
+        </>
+    )
+};
+
+export default Services;
