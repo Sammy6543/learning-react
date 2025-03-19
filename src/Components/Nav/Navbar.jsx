016f7d193557baf927683c0bdd61c70b7c26b1d2
Sammy6543 <mohammedsameerrangrej.cs19@aryacollege.in>
2025-03-19 23:26:17 +0530

    navbar created with logo and links

--- src/Components/Nav/Navbar.jsx
+++ src/Components/Nav/Navbar.jsx
@@ -1,9 +1,34 @@
 import "./navbar.css";
+import logo from "../../Assets/logo.png";
 
 const Navbar = () => {
     return (
         <>
-            <h1>Navbar</h1>
+            <div className="top-nav">
+                <div className="left">
+                    Monday-Saturday, 8AM to 10PM
+                </div>
+                <div className="right">
+                    Call us now +91 77378 75786
+                </div>
+            </div>
+            <div className="bottom-nav">
+                <div className="nav-logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="nav-links">
+                    <ul>
+                        <li><a href="#home" className="active">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#departments">Departments</a></li>
+                        <li><a href="#doctors">Doctors</a></li>
+                        <li><a href="#dropdown">Dropdown</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                    <button>Make an Appointment</button>
+                </div>
+            </div>
         </>
     )
 };
